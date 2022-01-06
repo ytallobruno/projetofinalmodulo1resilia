11c1209eb521f0c646c94d0a93be0720c38169d6
Ytallo Bruno <ytallobruno@hotmail.com>
2022-01-05 21:46:51 -0300

    first commit

---
+++ personagens/Presto/presto.js
@@ -0,0 +1,17 @@
+function primeiroNivelPresto() {
+  while (question != 1 || question != 2) {
+    var question = prompt(
+      '1. Olha em volta e presta atenção na situação que se encontra\n2. "Não, não, tudo bem! Daremos o dinheiro"'
+    );
+
+    if (question == 1) {
+      window.location.href = "./presto-pt-2.html";
+      break;
+    } else if (question == 2) {
+      window.location.href = "./gameoverpresto1.html";
+      break;
+    } else {
+      alert("Digite 1 ou 2");
+    }
+  }
+}
